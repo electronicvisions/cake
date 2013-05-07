ffe036ae33f0d0cf3ffff59ee69cee3f439fd568
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2013-05-07 17:38:07 +0200

    improved docstrings and comments

--- source/pycairo/simulator.py
+++ source/pycairo/simulator.py
@@ -1,12 +1,12 @@
-'''Simulation of the AdEx neuron model by simple integration over a given timestep.'''
+"""Simulation of the AdEx neuron model by simple integration over a given timestep."""
 
 import math
 import numpy as np
-import pylab
-import sys
+import matplotlib.pyplot as plt
 
-class Neuron:
-    '''This class defines the simulation of the AdEx neuron model.'''
+class Neuron(object):
+    """This class defines the simulation of the AdEx neuron model."""
+
     def __init__(self):
         # default set of parameters, will usually be overwritten
         self.parameters = {"EL" : 700.0,
@@ -32,8 +32,8 @@ class Neuron:
         self.reset()
 
     ## Main function for the simulation of the AdEx neuron
-    def sim(self,timestep,stim,spikes_x=[],spikes_i=[],ramp_current=0):
-        '''Perform a single timestep.
+    def sim_step(self,timestep,stim,spikes_x=[],spikes_i=[],ramp_current=0):
+        """Perform a single timestep.
 
         Args:
             timestep: simulation timestep in seconds
@@ -41,7 +41,7 @@ class Neuron:
             spikes_x: optional list of excitatory spikes
             spikes_i: optional list of excitatory spikes
             ramp_current: optional ramp current in A
-        '''
+        """
 
         # Incoming spikes (excitatory)
         if (int(self.time_count/timestep) in spikes_x):
@@ -84,9 +84,10 @@ class Neuron:
         self.time_count = self.time_count + timestep
 
     def reset(self):
-        '''Reset the simulation.'''
+        """Reset the simulation."""
+
         # runtime variables
-        self.v = self.parameters['EL']*1e-3
+        self.v = self.parameters['EL']*1e-3 # convert mV to V
         self.w = 0.
         self.gsynx = 0.
         self.gsyni = 0.
@@ -101,18 +102,25 @@ class Neuron:
         self.w_record = []
         self.gsynx_record = []
         self.gsyni_record = []
-    
+
 class Simulator:
+    """Performs simulations for given parameters to get frequencies etc."""
 
     def __init__(self):
-        
-        # Create neuron
-        self.soma = Neuron()
-        
+        """Initialize Simulator.
+
+        Create Neuron object for simulations. Set default options.
+        """
+
+        self.neuron = Neuron()
+        self.current_ramp = 0
+
+        ### default options below ###
+
         # Parameters variation
         self.parameters_variation = False    
         self.variation = 1e-3
-        
+
         # Noise
         self.noise = False
         self.noise_std = 10e-3
@@ -121,78 +129,85 @@ class Simulator:
         self.jitter = False
         self.jitter_std = 1e-8
 
-        # Current ramp value
-        self.current_ramp = 0
-
     def sim(self,time,stim,params,spikes_x=[],spikes_i=[],timestep=1e-8,ramp_current=0):
+        """Perform a single simulation run for given parameters.
 
-        # Create time array
-        time_array = np.arange(0,time,timestep)
+        Args:
+            time: end time of simulation run
+            stim: neuron current stimulus in A
+            params (dict): parameters and values that differ from Neuron's default parameters
+            spikes_x (optional list): times of excitatory spikes
+            spikes_i (optional list): times of inhibitory spikes
+            timestep (optional): simulation timestep
+            ramp_current (optional): maximum current, ramped from 0 to this value over full simulation time
 
-        # Apply parameters variations
-        if (self.parameters_variation == True):
+        Returns:
+            (time, voltage, spikes)
+
+            time: list of simulation timesteps
+            voltage: list of voltages at each timestep
+            spikes: list of detected spikes
+        """
+
+        time_array = np.arange(0,time,timestep) # array containing each timestep
+
+        if self.parameters_variation:
+            # Apply parameters variations
             for i in params:
                 params[i] = np.random.normal(params[i],self.variation*params[i])
-        
-        # Apply parameters
-        self.soma.parameters = params
-        
-        # Reset neuron
-        self.soma.reset()
-        
-        # Store stimulus
-        applied_stim = stim
-        
+
+        self.neuron.parameters = params # apply parameters
+
+        self.neuron.reset()
+
+        applied_stim = stim # Store stimulus
+
         # Simulate
         for i in time_array:
-
             # Generate current ramp
             self.current_ramp = ramp_current*i/time
-    
-            # Simulation step
-            self.soma.sim(timestep,applied_stim,spikes_x,spikes_i,self.current_ramp)
-            
-        # Add noise
-        if (self.noise == True):
-            for i,item in enumerate(self.soma.v_record):
-                self.soma.v_record[i] = self.soma.v_record[i] + np.random.normal(0,self.noise_std)
 
-        # Add jitter 
-        if (self.jitter == True):
-            for i,item in enumerate(self.soma.spikes):
-                self.soma.spikes[i] = np.random.normal(self.soma.spikes[i],self.jitter_std)
+            # Simulation step
+            self.neuron.sim_step(timestep,applied_stim,spikes_x,spikes_i,self.current_ramp)
+
+        if self.noise: # Add noise
+            for i,item in enumerate(self.neuron.v_record):
+                self.neuron.v_record[i] = self.neuron.v_record[i] + np.random.normal(0,self.noise_std)
+
+        if self.jitter: # Add jitter
+            for i,item in enumerate(self.neuron.spikes):
+                self.neuron.spikes[i] = np.random.normal(self.neuron.spikes[i],self.jitter_std)
 
         self.current_ramp = 0
-        
-        return time_array, self.soma.v_record, self.soma.spikes
-    
-    # Plot a simulation   
-    def plot_sim(self,time,stim,params,spikes_x=[],spikes_i=[]):
-       
-        t,v,spikes = self.sim(time,stim,params,spikes_x,spikes_i)
-        pylab.plot(t,v,linewidth = 1.0)
 
-        #pylab.show()
-        
-    # Plot spikes only
+        return time_array, self.neuron.v_record, self.neuron.spikes
+
+    def plot_sim(self,time,stim,params,spikes_x=[],spikes_i=[]):
+        """Run and plot a simulation. See sim()"""
+
+        t,v,spikes = self.sim(time,stim,params,spikes_x,spikes_i)
+        plt.plot(t,v,linewidth = 1.0)
+
     def plot_spikes(self,time,stim,params):
-       
+        """Run a simulation, plot spikes only. See sim()"""
+
         t,v,spikes = self.sim(time,stim,params)
         for i in spikes:
-            pylab.plot([i,i],[0,1],c='k')
-        
-        pylab.ylim(-5,6)
-        #pylab.show()
-   
-    # Return only spike times    
+            plt.plot([i,i],[0,1],c='k')
+
+        plt.ylim(-5,6)
+
     def get_spike_time(self,time,stim,params):
+        """Return only spike times"""
+
         t,v,spikes = self.sim(time,stim,params)
         return spikes
-   
-    # Return an array with spiking frequency   
+
     def compute_freq(self,time,stim,params):
+        """Return an array with spiking frequency"""
+
         t,v,spikes = self.sim(time,stim,params)
-   
+
         r = len(spikes)
         for i in range(1,r):
             p = spikes[i]-spikes[i-1]
@@ -201,49 +216,51 @@ class Simulator:
             freq = 1/p
         except:
             freq = 0
-          
+
         return freq
-        
-    # Return an array with spiking frequency   
+
     def compute_psp_integral(self,time,stim,params):
-        
+        """Return an array with spiking frequency"""
+
         t,v,spikes = self.sim(time,stim,params,[100])
-        
+
         for j,item in enumerate(v):
             v[j] = v[j]*1e3
         integral = np.trapz(t,v)
-          
+
         return integral
-   
-    # Plot the spiking frequency versus time   
+
     def plot_freq(self,time,stim,params):
+        """Plot the spiking frequency versus time"""
+
         t,v,spikes = self.sim(time,stim,params)
-        
+
         freqs = []
         for i in range(1,len(spikes)):
             freqs.append(1/(spikes[i]-spikes[i-1]))
-            
-        pylab.plot(freqs)
-        #pylab.show()
-   
-    # Return time and frequency arrays   
+
+        plt.plot(freqs)
+
     def get_time_freq(self,time,stim,params):
+        """Return time and frequency arrays"""
+
         t,f = self.compute_freq(time,stim,params)
         last = len(f) - 1
         f.append(f[last])
         return t,f
-    
-    # Return the stationnary spiking frequency   
+
     def get_stat_freq(self,time,stim,params):
+        """Return the stationary spiking frequency"""
+
         eps = 0.1
         limit = 0
         limitfound = False
-        
+
         t,v,spikes = self.sim(time,stim,params)
         freqs = []
         for i in range(1,len(spikes)):
             freqs.append(1/(spikes[i]-spikes[i-1]))
-            
+
         for i in range(1,len(freqs)):
             diff = freqs[i]-freqs[i-1]
             if (diff < eps):
@@ -252,27 +269,28 @@ class Simulator:
         if (limitfound == False):
             print "No limit found !"
         return limit
-   
-    # Return the discrete derivative   
+
     def compute_deriv(self,array):
+        """Return the discrete derivative"""
+
         deriv = range(len(array))
         for i in range(len(array)-1):
             deriv[i] = array[i+1] - array[i]
         return deriv
-        
 
-    # Return the relation between gL and frequency
+
     def get_gl_freq(self,gLMin,gLMax,EL,Vreset,Vt):
-        
+        """Return the relation between gL and frequency"""
+
         # Number of points
         s = 3
-        
+
         # Frequency array
         freq = []
 
         # Generate gL values
         values = np.arange(gLMin,gLMax+(gLMax-gLMin)/s,(gLMax-gLMin)/s)
-      
+
         # Calculate stationnary frequency for each gL  
         for i in values:
             parameters = {"EL" : EL,    
@@ -294,34 +312,25 @@ class Simulator:
             "Esynx" : 1e-6,
             "Esyni" : 1e-6}
             freq.append(self.compute_freq(30e-6,0,parameters))
-            self.soma.reset()
-        
+            self.neuron.reset()
+
         # Calculate fit
         a,b,c = np.polyfit(freq,values,2)
-        
-        # For debug : plot fit
-        # valuesC = []
-        # for i in freq:
-            # valuesC.append(a*i*i+b*i+c)
-            
-        # pylab.scatter(freq,values)
-        # pylab.plot(freq,valuesC)
-        # pylab.show()
-        
+
         return a,b,c
-        
+
     # # Return the relation between gL and frequency
     # def getTausynPSP(self,paramMin,paramMax,EL,Vreset,Vt,gL,Esynx):
-        
+
     #     # Number of points
     #     s = 5
-        
+
     #     # PSP array
     #     psp = []
 
     #     # Generate values
     #     values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
     #     for i in values:
     #         parameters = {"EL" : EL,    
     #         "gL" : gL,
@@ -339,34 +348,25 @@ class Simulator:
     #         "tausynx" : i,
     #         "Esynx" : Esynx}
     #         psp.append(self.computePSPIntegral(100e-6,0,parameters))
-    #         self.soma.reset()
-        
+    #         self.neuron.reset()
+
     #     # Calculate fit
     #     a,b,c = np.polyfit(psp,values,2)
-        
-    #     # For debug : plot fit
-    #     # valuesC = []
-    #     # for i in psp:
-    #         # valuesC.append(a*i*i+b*i+c)
-            
-    #     # pylab.scatter(psp,values)
-    #     # pylab.plot(psp,valuesC)
-    #     # pylab.show()
-        
+
     #     return a,b,c
-        
-    # Return the relation between a and frequency
+
     def get_a_freq(self,paramMin,paramMax,EL,Vreset,Vt,gL):
-        
+        """Return the relation between a and frequency"""
+
         # Number of points
         s = 3
-        
+
         # Frequency array
         freq = []
 
         # Generate values
         values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
         # Calculate stationnary frequency
         for i in values:
             parameters = {"EL" : EL,    
@@ -388,34 +388,25 @@ class Simulator:
             "Esynx" : 1e-6,
             "Esyni" : 1e-6}
             freq.append(self.get_stat_freq(200e-6,0,parameters))
-            self.soma.reset()
-        
+            self.neuron.reset()
+
         # Calculate fit
         a,b,c = np.polyfit(freq,values,2)
-        
-        # For debug : plot fit
-        # valuesC = []
-        # for i in freq:
-            # valuesC.append(a*i*i+b*i+c)
-            
-        # pylab.scatter(freq,values)
-        # pylab.plot(freq,valuesC)
-        # pylab.show()
-        
+
         return a,b,c
-        
-    # Return the relation between a and frequency
+
     def get_vexp_freq(self,paramMin,paramMax,EL,Vreset,Vt,gL,dT):
-        
+        """Return the relation between a and frequency"""
+
         # Number of points
         s = 20
-        
+
         # Frequency array
         freq = []
 
         # Generate values
         values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
         # Calculate stationnary frequency
         for i in values:
             parameters = {"EL" : EL,    
@@ -437,8 +428,8 @@ class Simulator:
             "Esynx" : 1e-6,
             "Esyni" : 1e-6}
             freq.append(self.get_stat_freq(200e-6,0,parameters))
-            self.soma.reset()
-            
+            self.neuron.reset()
+
         # Cut constant part
         thresh = 10e3
         new_freq = []
@@ -447,10 +438,10 @@ class Simulator:
             if (math.fabs(freq[i] - freq[i-1]) > thresh):
                 new_freq.append(freq[i])
                 new_values.append(values[i])
-                
+
         freq = new_freq
         values = new_values
-                
+
         # Cut constant part
         thresh = 1e6
         new_freq = []
@@ -459,37 +450,28 @@ class Simulator:
             if (freq[i] < thresh):
                 new_freq.append(freq[i])
                 new_values.append(values[i])
-                
+
         # print freq
         # print new_freq
         # print new_values
-        
+
         # Calculate fit
         a,b,c = np.polyfit(new_freq,new_values,2)
-        
-        # For debug : plot fit
-        valuesC = []
-        for i in new_freq:
-            valuesC.append(a*i*i+b*i+c)
-            
-        pylab.scatter(new_freq,new_values)
-        pylab.plot(new_freq,valuesC)
-        pylab.show()
-        
+
         return a,b,c
-        
-    # Return the relation between a and frequency
+
     def get_b_freq(self,paramMin,paramMax,EL,Vreset,Vt,gL,a,tauw):
-        
+        """Return the relation between a and frequency"""
+
         # Number of points
         s = 3
-        
+
         # Frequency array
         freq = []
 
         # Generate gL values
         values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
         # Calculate stationnary frequency for each gL  
         for i in values:
             parameters = {"EL" : EL,    
@@ -512,34 +494,25 @@ class Simulator:
             "Esynx" : 1e-6,
             "Esyni" : 1e-6}
             freq.append(self.get_stat_freq(100e-6,0,parameters))
-            self.soma.reset()
-        
+            self.neuron.reset()
+
         # Calculate fit
         a,b,c = np.polyfit(freq,values,2)
-        
-        # For debug : plot fit
-        # valuesC = []
-        # for i in freq:
-            # valuesC.append(a*i*i+b*i+c)
-            
-        # pylab.scatter(freq,values)
-        # pylab.plot(freq,valuesC)
-        # pylab.show()
-        
+
         return a,b,c
-        
-    # Return the relation between gL and frequency
+
     def get_tauw_isi(self,paramMin,paramMax,EL,Vreset,Vt,gL,gLadapt,stim):
-        
+        """Return the relation between gL and frequency"""
+
         # Number of points
         s = 10
-        
+
         # Frequency array
         ISI = []
 
         # Generate gL values
         values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
         # Calculate stationnary frequency for each gL  
         for i in values:
             parameters = {"EL" : EL,    
@@ -566,37 +539,28 @@ class Simulator:
             #ISI.append(spikes[1]-spikes[0])
             #print parameters['tw']
             #print spikes
-            self.soma.reset()
-            
+            self.neuron.reset()
+
         # for i in range(len(values)):
             # values[i] = float(1/float(values[i]))
-        
+
         # Calculate fit
         a,b,c,d,e = np.polyfit(ISI,values,4)
-            
-        #For debug : plot fit
-        # valuesC = []
-        # for i in ISI:
-            # valuesC.append(a*i*i*i*i+b*i*i*i+c*i*i+d*i+e)
-            
-        # pylab.scatter(ISI,values)
-        # pylab.plot(ISI,valuesC)
-        # pylab.show()
-        
+
         return a,b,c,d,e
-        
-        # Return the relation between gL and frequency
+
     def get_tauw_int(self,paramMin,paramMax,EL,Vreset,Vt,gL,gLadapt,stim):
-        
+        """Return the relation between gL and frequency"""
+
         # Number of points
         s = 10
-        
+
         # Integral array
         Int = []
 
         # Generate gL values
         values = np.arange(paramMin,paramMax+(paramMax-paramMin)/s,(paramMax-paramMin)/s)
-      
+
         # Calculate stationnary frequency for each gL  
         for i in values:
             parameters = {"EL" : EL,    
@@ -616,23 +580,23 @@ class Simulator:
             "Esynx" : 1e-6}
             t,v,spikes = self.sim(50e-6,stim,parameters)
             Int.append(np.trapz(t,v))
-            self.soma.reset()
-        
+            self.neuron.reset()
+
         # Calculate fit
         a,b,c = np.polyfit(Int,values,2)
-            
-        #For debug : plot fit
-        # valuesC = []
-        # for i in ISI:
-            # valuesC.append(a*i*i*i*i+b*i*i*i+c*i*i+d*i+e)
-            
-        # pylab.scatter(ISI,values)
-        # pylab.plot(ISI,valuesC)
-        # pylab.show()
-        
+
         return a,b,c
 
     def smooth(self,v,width):
+        """Smooth voltage array after given width.
+
+        Args:
+            v (list): voltages
+            width: list entry at which smoothing should start
+
+        Returns:
+            smoothed voltage list
+        """
 
         s_v = []
         for i in range(width-1):
