db09f07cf713ce8ad068decb7e4e634668cedfb4
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2013-05-17 15:04:25 +0200

    add units module

---
+++ source/pycairo/units.py
@@ -0,0 +1,26 @@
+"""Units for variables, using unum.
+
+Example:
+    import pycairo.units as units
+
+    i = 5*units.uA
+    print i.asUnit(units.nA)
+    print i.asNumber()
+    print i.asUnit(units.nA).asNumber()
+"""
+
+from unum import Unum
+import unum.units
+
+mV = Unum.unit('mV', 1e-3 * unum.units.V, 'millivolt')
+
+nF = Unum.unit('nF', 1e-9 * unum.units.F, 'nanofarad')
+pF = Unum.unit('pF', 1e-12 * unum.units.F, 'picofarad')
+
+nS = Unum.unit('nS', 1e-9 * unum.units.S, 'nanosiemens')
+
+ms = unum.units.ms
+us = unum.units.us
+
+uA = unum.units.uA
+nA = unum.units.nA
