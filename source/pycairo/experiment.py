02780b6282d56f46787db332bf56e6bce0612661
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2013-06-25 17:48:17 +0200

    BaseExperiment is running now, see notebook

--- source/pycairo/experiment.py
+++ source/pycairo/experiment.py
@@ -35,12 +35,15 @@ class Current(Unit):
         super(Current, self).__init__(float(value), apply_calibration)
 
     @classmethod
-    def _check(value):
+    def _check(self, value):
         if value < 0. or value > 2500.:
-            raise ValueError("Current value {} out of range".format(value))
+            raise ValueError("Current value {} nA out of range".format(value))
 
     def toDAC(self):
         return DAC(self.value/2500.*1023., self.apply_calibration)
+
+    def __repr__(self):
+        return "{} nA".format(self.value)
 
 
 class Voltage(Unit):
@@ -48,8 +51,16 @@ class Voltage(Unit):
     def __init__(self, value, apply_calibration=False):
         super(Voltage, self).__init__(float(value), apply_calibration)
 
+    @classmethod
+    def _check(self, value):
+        if value < 0. or value > 1800.:
+            raise ValueError("Voltage value {} mV out of range".format(value))
+
     def toDAC(self):
         return DAC(self.value/1800.*1023., self.apply_calibration)
+
+    def __repr__(self):
+        return "{} mV".format(self.value)
 
 
 class DAC(Unit):
@@ -57,7 +68,7 @@ class DAC(Unit):
         super(DAC, self).__init__(int(value), apply_calibration)
 
     @classmethod
-    def _check(value):
+    def _check(self, value):
         if not isinstance(value, int):
             raise TypeError("DAC value is no integer")
         if value < 0 or value > 1023:
@@ -66,6 +77,9 @@ class DAC(Unit):
     def toDAC(self):
         return self
 
+    def __repr__(self):
+        return "{}".format(self.value)
+
 
 class BaseExperiment(object):
     def __init__(self):
@@ -73,34 +87,136 @@ class BaseExperiment(object):
         self.adc = None
 
     def get_parameters(self):
-        return []
+        """Return neuron parameters for this experiment. Values can be of type Current, Voltage or DAC."""
 
-    def get_parameters_to_calibrate(self):
-        return []
+        return {'E_l': Voltage(1000),
+                'E_syni': Voltage(900),
+                'E_synx': Voltage(900),
+                'I_bexp': Current(0),
+                'I_convi': Current(1000),
+                'I_convx': Current(1000),
+                'I_fire': Current(0),
+                'I_gl': Current(400),
+                'I_gladapt': Current(0),
+                'I_intbbi': Current(2000),
+                'I_intbbx': Current(2000),
+                'I_pl': Current(2000),
+                'I_radapt': Current(2000),
+                'I_rexp': Current(750),
+                'I_spikeamp': Current(2000),
+                'V_exp': Voltage(536),
+                'V_syni': Voltage(1000),
+                'V_syntci': Voltage(900),
+                'V_syntcx': Voltage(900),
+                'V_synx': Voltage(1000),
+                'V_t': Voltage(1000),  # recommended threshold, maximum is 1100
+                'V_reset': Voltage(500)  # shared between all neurons
+                }
 
-    def measure(self):
-        pass
+    def prepare_parameters(self, parameters, step, neuron_ids):
+        """Prepare parameters before writing them to the hardware.
 
-    def prepare_parameters(self, parameters, step):
-        # apply parameters
-        pass
+        This includes converting to DAC values, applying calibration and
+        merging of the step specific parameters.
 
-    def prepare_measurement(self, hicann_handle):
+        Calibration can be different for each neuron, which leads to different DAC values
+        for each neuron, even though the parameter value is the same.
+
+        Args:
+            parameters: dict of all neuron parameters and values of type Current, Voltage or DAC
+            step: dict of all neuron parameters different in this specific step
+            neuron_ids: list of neuron ids that should get the parameters
+
+        Returns:
+            dict of neuron ids -> dict of neuron parameters -> DAC values (int)
+
+            Example: {0: {'E_l': 400, ...},
+                      1: {'E_l': 450, ...}}
+        """
+
+        neuron_step_parameters = {}
+        for neuron_id in neuron_ids:
+            neuron_step_parameters[neuron_id] = {}
+
+        for p in parameters:
+            if p in step:
+                continue  # skip parameter
+            value = parameters[p].toDAC().value
+            apply_calibration = parameters[p].apply_calibration
+            for neuron_id in neuron_ids:
+                if apply_calibration:
+                    # apply calibration
+                    calibrated_value = value  # TODO
+                    neuron_step_parameters[neuron_id][p] = calibrated_value
+                else:
+                    neuron_step_parameters[neuron_id][p] = value
+
+        for p in step:
+            value = step[p].toDAC().value
+            apply_calibration = step[p].apply_calibration
+            for neuron_id in neuron_ids:
+                if apply_calibration:
+                    # apply calibration
+                    calibrated_value = value  # TODO
+                    neuron_step_parameters[neuron_id][p] = calibrated_value
+                else:
+                    neuron_step_parameters[neuron_id][p] = value
+
+        return neuron_step_parameters
+
+    def prepare_measurement(self, neuron_parameters):
         """Prepare measurement.
         Perform reset, write general hardware settings.
         """
-        pass
+        self.hicann.reset()
+        self.hicann.configure()
+        self.hicann.program_fg(neuron_parameters)
 
-    def run_experiment(self, hicann_handle):
+    def get_repetitions(self):
+        """How many times should each step be repeated?"""
+        return 1
+
+    def get_steps(self):
+        """Measurement steps for sweeping.
+
+        Returns:
+            list of parameter dicts for each step
+
+            Example: [{'E_l': Voltage(400)}, {'E_l': Voltage(600)}, {'E_l': Voltage(800)}]
+        """
+        return [{}]  # single step using default parameters
+
+    def get_neurons(self):
+        """Which neurons should this experiment run on?
+
+        All neurons will be prepared with the same parameters (except for calibration differences)
+        and each neuron will be measured in the measure step.
+        """
+
+        return [0]  # only neuron 0
+
+    def run_experiment(self):
         parameters = self.get_parameters()
+        neuron_ids = self.get_neurons()
+        self.results = []
         for step in self.get_steps():
-            step_parameters = self.prepare_parameters(parameters, step)
-            for r in self.get_repetions():
-                self.prepare_measurement(hicann_handle)
-                self.hicann.program_fg(step_parameters)
-                # TODO where does the neuron loop go?
-                self.measure(step, hicann_handle)
+            step_parameters = self.prepare_parameters(parameters, step, neuron_ids)
+            for r in range(self.get_repetitions()):
+                self.prepare_measurement(step_parameters)
+                self.results.append(self.measure(step, neuron_ids))
         self.process_results()
+
+    def measure(self, step, neuron_ids):
+        results = {}
+        for neuron_id in neuron_ids:
+            self.hicann.activate_neuron(neuron_id)
+            self.hicann.enable_analog_output(neuron_id)
+            t, v = self.adc.measure_adc(1000)
+            results[neuron_id] = v
+        return results
+
+    def process_results(self):
+        pass  # no processing
 
 
 class Calibrate_E_l(BaseExperiment):
