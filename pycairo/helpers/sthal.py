b456265679b40a302ff741317308c8be5a24bd8e
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2013-10-07 13:44:41 +0200

    no FPGA reset when switching neurons

--- pycairo/helpers/sthal.py
+++ pycairo/helpers/sthal.py
@@ -2,6 +2,10 @@ import pysthal
 
 
 class UpdateAnalogOutputConfigurator(pysthal.HICANNConfigurator):
+    def config_fpga(self, *args):
+        # do not reset FPGA
+        pass
+
     def config(self, fpga_handle, h, hicann):
         self.config_neuron_quads(h, hicann)
         self.config_analog_readout(h, hicann)
