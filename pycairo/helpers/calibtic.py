baa8228ca4ffb2049c40c890206a6052be999e11
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2013-10-15 15:58:05 +0200

    backend helper

--- pycairo/helpers/calibtic.py
+++ pycairo/helpers/calibtic.py
@@ -1,6 +1,18 @@
 import pycalibtic
 
 
+def init_backend(type='xml', path='/afs/kip.uni-heidelberg.de/user/mkleider/tmp/calibtic-xml-backend'):
+    # FIXME hardcoded path
+    if type == 'xml':
+        lib = pycalibtic.loadLibrary('libcalibtic_xml.so')
+        backend = pycalibtic.loadBackend(lib)
+        backend.config('path', path)
+        backend.init()
+        return backend
+    else:
+        raise ValueError("unknown backend type")
+
+
 def create_pycalibtic_polynomial(coefficients):
     """Create a pycalibtic.Polynomial from a list of coefficients.
 
