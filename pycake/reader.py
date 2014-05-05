0785ef4b24d0016c14766f1be919dec76bf08360
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2014-05-05 15:00:46 +0200

    pass all hist related options by kwargs

--- pycake/reader.py
+++ pycake/reader.py
@@ -57,10 +57,10 @@ class Reader(object):
             results[neuron] = [r[neuron][key] for r in ex.results]
         return results
 
-    def plot_hist(self, parameter, key, step, repetition=0, histrange = 20, **kwargs):
+    def plot_hist(self, parameter, key, step, repetition=0, **kwargs):
         results = self.get_results(parameter, self.get_neurons(), key, repetition)
         results_list = np.array(results.values())[:,step]
-        return plt.hist(results_list, bins=histrange, **kwargs)
+        return plt.hist(results_list, **kwargs)
 
 
 
