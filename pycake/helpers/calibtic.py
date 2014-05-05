4c83737c9b539eca651bf5b0706fde45249c2be0
Dominik Schmidt <dominik.schmidt@kip.uni-heidelberg.de>
2014-05-06 00:02:47 +0200

    Revert "fix check for missing calibration" Added exists function to calibtic (please update calibtic!) I want to avoid all the Exception handling, so I added a function to check if a neuron calibration contains data for a specific parameter.
    This reverts commit 96b63c0992cc70d32b0ceb7ae0330b72cbfc5558.

--- pycake/helpers/calibtic.py
+++ pycake/helpers/calibtic.py
@@ -214,11 +214,11 @@ class Calibtic(object):
         if not calib:
             return int(round(dac_value))
 
-        try:
+        if calib.exists(parameter):
             calib_dac_value = calib.at(parameter).apply(dac_value)
             self.logger.TRACE("Calibrated {} parameter {}: {} --> {} DAC".format(coord, parameter.name, dac_value, calib_dac_value))
-        except Exception as e:
-            self.logger.WARN("Applying calibration failed \"{}\": Nothing found for {} parameter {}.".format(e, coord, parameter.name))
+        else:
+            self.logger.WARN("Applying calibration failed: Nothing found for {} parameter {}.".format(coord, parameter.name))
             calib_dac_value = dac_value
 
         if calib_dac_value < 0:
