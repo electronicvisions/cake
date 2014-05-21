01e4d0d29dbec8242c88ce8b0e5ac1d9ef7c3c56
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2014-05-21 11:36:50 +0200

    allow to retrieve and plot results from a given repetition

--- pycake/reader.py
+++ pycake/reader.py
@@ -43,29 +43,35 @@ class Reader(object):
             neuron = C.NeuronOnHICANN(C.Enum(neuron))
         return [r[neuron].get(key, None) for r in ex.results]
 
-    def get_results(self, parameter, neurons, key):
+    def get_results(self, parameter, neurons, key, repetition=None):
         """ Get measurement results for one neuron.
 
             Args:
                 parameter: which parameter?
                 neurons: list of neuron coordinates or ids
                 key: which key? (e.g. 'mean', 'std', 'baseline', ...)
-                repetition: single repetition or mean over all? (mean not yet implemented)
+                repetition: if not None only values for given repetition are returned
 
             Returns:
                 {neuron: [results]} if neurons
         """
         ex = self.runner.experiments[parameter]
+        nsteps = len(self.runner.config.copy(parameter).get_steps())
 
         results = {}
         for neuron in neurons:
             if isinstance(neuron, int):
                 neuron = C.NeuronOnHICANN(C.Enum(neuron))
+
             results[neuron] = [r[neuron].get(key, None) for r in ex.results]
+
+            if repetition != None:
+                results[neuron] = list(np.array(results[neuron])[:,[nsteps*repetition + step for step in range(nsteps)]])
+
         return results
 
-    def plot_hist(self, parameter, key, step, **kwargs):
-        results = self.get_results(parameter, self.get_neurons(), key)
+    def plot_hist(self, parameter, key, step, repetition=0, **kwargs):
+        results = self.get_results(parameter, self.get_neurons(), key, repetition)
         results_list = np.array(results.values())[:,step]
         hist = plt.hist(results_list, **kwargs)
         config = self.runner.config.copy(parameter)
