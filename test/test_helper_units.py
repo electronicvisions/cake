8feeff6baac6108eac1ae747db553571be9c2afc
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2014-09-18 17:08:19 +0200

    refactor

--- test/test_helper_units.py
+++ test/test_helper_units.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+This test makes sure that currents and voltages are correctly converted to DAC
+and checks whether DAC, Voltage and Current have a toDAC() function at the
+same time.
+"""
+
 import unittest
 from pycake.helpers.units import Current, Voltage, DAC
 
 
-class TestExperiment(unittest.TestCase):
+class TestUnitsHelper(unittest.TestCase):
     def test_Current(self):
         c = Current(100)
         self.assertEqual(c.toDAC().value, 41)
