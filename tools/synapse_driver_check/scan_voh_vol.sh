043fa148a0a1887c7b1f75253c7e21390fff2996
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2014-11-04 15:16:01 +0100

    scan also frequency and bkg isi

--- tools/synapse_driver_check/scan_voh_vol.sh
+++ tools/synapse_driver_check/scan_voh_vol.sh
@@ -6,29 +6,38 @@ measure() {
 
 	voh_dac=$1
 	vol_dac=$2
-
-	#date=2014-09-18
+	freq=$3
+	bkgisi=$4
 
 	extrasuffix=${run}_vohdac_${voh_dac}_voldac_${vol_dac}
 
-	$dry srun -p wafer defects.py 288 --extrasuffix $extrasuffix --wafer 1 --ana 2>&1 | tee log_${extrasuffix}
+	$dry srun -p wafer defects.py 288 --extrasuffix $extrasuffix --wafer 1 --freq $freq --bkgisi $bkgisi --ana 2>&1 | tee log_${extrasuffix}
 
 }
 
-run=run_7_date_`date -I`
+run=run_10_date_`date -I`
 
-for voh_dac in `seq 0 10 100`
+best_voh_dac=0
+best_vol_dac=50
+
+for bkgisi in `seq 5000 2000 15000`
 do
-	for vol_dac in `seq 0 10 40`
+	for freq in 125
 	do
-		if [[ $voh_dac -ge $vol_dac ]]
-		then
-			echo "continue"
-			continue
-		fi
-		echo "at voh DAC ${voh_dac} and vol DAC ${vol_dac}"
-		$dry ../l1_voltages.py 10 $voh_dac
-		$dry ../l1_voltages.py 9 $vol_dac
-		measure $voh_dac $vol_dac
+		for voh_dac in $best_voh_dac # run 8 `seq 0 2 6`
+		do
+			for vol_dac in $best_vol_dac # run 8`seq 40 -5 0`
+			do
+				if [[ $voh_dac -ge $vol_dac ]]
+				then
+					echo "continue"
+					continue
+				fi
+				echo "at voh DAC ${voh_dac} and vol DAC ${vol_dac}"
+				$dry ../l1_voltages.py 10 $voh_dac
+				$dry ../l1_voltages.py 9 $vol_dac
+				measure $voh_dac $vol_dac $freq $bkgisi
+			done
+		done
 	done
 done
