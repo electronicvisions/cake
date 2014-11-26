7a6e55efae49de1fa86604a400ca2327c2f1f329
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2014-11-26 17:26:29 +0100

    test mkdir

--- pycake/helpers/misc.py
+++ pycake/helpers/misc.py
@@ -1,11 +1,14 @@
 import os
 import errno
 
+
 # http://stackoverflow.com/a/600612/1350789
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:  # pragma: no cover
+            # unexpected, re-raise exception
+            raise
---
+++ test/test_helpers_misc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import random
+import string
+import os
+from pycake.helpers.misc import mkdir_p
+
+
+class TestMkdir(unittest.TestCase):
+    """Create random directory which did not exist,
+    create it again to make sure this does not cause issues,
+    remove it afterwards."""
+
+    def setUp(self):
+        def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+            return "".join(random.choice(chars) for _ in range(size))
+
+        # generate random directory name
+        randdir = os.path.join("/tmp", id_generator())
+        while(os.path.exists(randdir)):
+            # generate another one until it does not exist
+            randdir = os.path.join("/tmp", id_generator())
+
+        self.randdir = randdir
+
+    def test_mkdir(self):
+        randdir = self.randdir
+        self.assertFalse(os.path.exists(randdir))
+        mkdir_p(randdir)
+        self.assertTrue(os.path.isdir(randdir))
+
+        # assert this does not raise
+        mkdir_p(randdir)
+
+    def tearDown(self):
+        # cleanup
+        try:
+            os.rmdir(self.randdir)
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    unittest.main()
