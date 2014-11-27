cc8f33bc05076a395d9936645f0098165a2655cd
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2014-11-27 11:47:41 +0100

    test redman helper

--- test/test_helpers_redman.py
+++ test/test_helpers_redman.py
@@ -2,18 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """
+Test the redman helper.
 """
 
 import unittest
 import pickle
+import os
+import shutil
+import tempfile
+
 from pycake.helpers.redman import Redman
 from pyhalbe import Coordinate as C
 
 
 class TestRedmanHelper(unittest.TestCase):
+    def setUp(self):
+        self.basedir = tempfile.mkdtemp()
+
     def test_init(self):
-        hicann = C.HICANNGlobal(C.Enum(280))
-        r = Redman("/tmp", hicann)
+        """init backend"""
+        hicann = C.HICANNGlobal(C.Enum(94))
+        r = Redman(self.basedir, hicann)
 
         # initializing magic type should raise
         self.assertRaises(ValueError, r.init_backend, "magic")
@@ -21,8 +30,34 @@ class TestRedmanHelper(unittest.TestCase):
         # try pickling and unpickling
         p = pickle.dumps(r)
         r2 = pickle.loads(p)
-        del r2
+        self.assertIsInstance(r2, Redman)
 
+    def test_init_nodir(self):
+        """init backend in non-existing directory"""
+        hicann = C.HICANNGlobal(C.Enum(315))
+        nonexistdir = os.path.join(self.basedir, "newdir")
+        self.assertFalse(os.path.exists(nonexistdir))
+        r = Redman(nonexistdir, hicann)
+        self.assertIsInstance(r, Redman)
+        self.assertTrue(os.path.isdir(nonexistdir))
+
+    def test_defects(self):
+        hicann = C.HICANNGlobal(C.Enum(11))
+        n99 = C.NeuronOnHICANN(C.Enum(99))
+        r = Redman(self.basedir, hicann)
+        r.clear_defects()
+        self.assertTrue(r.hicann_with_backend.neurons().has(n99))
+        r.write_defects([n99])
+        self.assertFalse(r.hicann_with_backend.neurons().has(n99))
+
+        # force reloading
+        r._loaded = False
+        r.write_defects([n99])
+        self.assertFalse(r.hicann_with_backend.neurons().has(n99))
+
+    def tearDown(self):
+        """cleanup"""
+        shutil.rmtree(self.basedir)
 
 if __name__ == "__main__":
     unittest.main()
