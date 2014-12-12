efd281995a164c191408e1ffc1dd12d9a16513fd
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2014-12-12 15:58:36 +0100

    test isi=0

--- test/test_logic_spikes.py
+++ test/test_logic_spikes.py
@@ -25,6 +25,8 @@ class TestSpikes(unittest.TestCase):
         spikes = np.array([1, 2, 3, 4, 5, 6, 7, 8])
         self.assertEqual(sp.spikes_to_frequency(spikes), 1)
         self.assertEqual(sp.spikes_to_frequency(2*spikes), 0.5)
+        spikes = np.array([3, 3, 3, 3, 3, 3])
+        self.assertEqual(sp.spikes_to_frequency(spikes), 0)
 
 
 if __name__ == "__main__":
