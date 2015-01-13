58fa80535eea564d6d8ee574d9db4df4982775b8
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2015-01-13 17:13:06 +0100

    sort alphabetically, add logic.utils

--- test/test_import_all.py
+++ test/test_import_all.py
@@ -3,19 +3,22 @@ Import all cake submodules here.
 
 Useful for test coverage report.
 Also ensures that dependencies are installed.
+
+Generate a list running
+$ tree pycake -P '*.py'
 """
 
 import pycake
 
 # base modules
 import pycake.analyzer
-import pycake.calibrator
-import pycake.experimentbuilder
-import pycake.reader
 import pycake.calibrationrunner
+import pycake.calibrator
 import pycake.config
+import pycake.experimentbuilder
 import pycake.experiment
 import pycake.measure
+import pycake.reader
 
 # calibration submodule
 import pycake.calibration
@@ -24,19 +27,19 @@ import pycake.calibration.vsyntc
 
 # helpers submodule
 import pycake.helpers
-import pycake.helpers.peakdetect
-import pycake.helpers.SignalToNoise
-import pycake.helpers.TracesOnDiskDict
 import pycake.helpers.calibtic
-import pycake.helpers.psp_fit
-import pycake.helpers.sthal
-import pycake.helpers.trafos
-import pycake.helpers.psp_shapes
-import pycake.helpers.StorageProcess
-import pycake.helpers.units
 import pycake.helpers.misc
+import pycake.helpers.peakdetect
+import pycake.helpers.psp_fit
+import pycake.helpers.psp_shapes
 import pycake.helpers.redman
+import pycake.helpers.SignalToNoise
+import pycake.helpers.sthal
+import pycake.helpers.StorageProcess
 import pycake.helpers.TraceAverager
+import pycake.helpers.TracesOnDiskDict
+import pycake.helpers.trafos
+import pycake.helpers.units
 import pycake.helpers.WorkerPool
 
 # logic submodule
@@ -44,3 +47,4 @@ import pycake.logic
 import pycake.logic.psps
 import pycake.logic.spikes
 import pycake.logic.taum_fit
+import pycake.logic.utils
