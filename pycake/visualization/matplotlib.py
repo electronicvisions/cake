a70f23ad82a606ae885b26ad7eff5ee097f73ee7
Mitja Kleider <mitja.kleider@kip.uni-heidelberg.de>
2015-03-06 14:26:14 +0100

    Add plotting helpers

---
+++ pycake/visualization/matplotlib.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Collection of matplotlib customizations for visualization."""
+
+import matplotlib as mpl
+
+
+def latex_mode(on=True):
+    """Enable LaTeX for labels"""
+    mpl.rc('text', usetex=on)
+
+
+class ScaledFormatter(mpl.ticker.OldScalarFormatter):
+    """Formats tick labels scaled by *dx* and shifted by *x0*."""
+    def __init__(self, dx=1.0, x0=0.0, **kwargs):
+        self.dx, self.x0 = dx, x0
+
+    def rescale(self, x):
+        return x / self.dx + self.x0
+
+    def __call__(self, x, pos=None):
+        xmin, xmax = self.axis.get_view_interval()
+        xmin, xmax = self.rescale(xmin), self.rescale(xmax)
+        d = abs(xmax - xmin)
+        x = self.rescale(x)
+        s = self.pprint_val(x, d)
+        return s
