11909151c77064ff07207d8c0e0df0ef317eca4c
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2015-04-07 16:05:54 +0200

    make use of localdir module to set LD_LIBRARY_PATH and PYTHONPATH

--- tools/run_test_calib.sh
+++ tools/run_test_calib.sh
@@ -5,17 +5,7 @@
 module load numpy/1.8.0
 module load pyfftw
 module load pynn
-
-if [ -z $LD_LIBRARY_PATH ]; then
-    export LD_LIBRARY_PATH=$PWD/lib
-else
-    export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
-fi
-if [ -z $PYTHONPATH ]; then
-    export PYTHONPATH=$PWD/lib
-else
-    export PYTHONPATH=$PWD/lib:$PYTHONPATH
-fi
+module load localdir
 
 find .
 
