97c658bebb0aee8cd858595e5edc50b863de78a6
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2015-04-07 16:08:32 +0200

    parametrize wafer, hicann and outdir

--- tools/run_test_calib.sh
+++ tools/run_test_calib.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ev
 
+WAFER=1
+HICANN=288
+OUTDIR=./build/results_calibration
 
 # c.f. cake/project.prj
 module load numpy/1.8.0
@@ -14,8 +17,8 @@ cp -v bin/tools/*_parameters.py .
 # issue 1621
 export PYTHONPATH=.:$PYTHONPATH
 
-bin/tools/run_calibration.py fastcalibration_parameters.py --wafer 1 --hicann 288 --outdir ./build/results_calibration
-bin/tools/run_calibration.py evaluation_parameters.py --wafer 1 --hicann 288 --outdir ./build/results_calibration
+bin/tools/run_calibration.py fastcalibration_parameters.py --wafer $WAFER --hicann $HICANN --outdir $OUTDIR
+bin/tools/run_calibration.py evaluation_parameters.py --wafer $WAFER --hicann $HICANN --outdir $OUTDIR
 
 find
 
