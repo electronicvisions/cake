8dcfca449604220c5942011be20c74260e3166e0
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2015-04-07 16:20:22 +0200

    make plots

--- tools/run_test_calib.sh
+++ tools/run_test_calib.sh
@@ -3,6 +3,10 @@
 WAFER=1
 HICANN=288
 OUTDIR=./build/results_calibration
+PLOTDIR=$OUTDIR/plots
+
+mkdir -p $OUTDIR
+mkdir -p $PLOTDIR
 
 # c.f. cake/project.prj
 module load numpy/1.8.0
@@ -22,4 +26,17 @@ bin/tools/run_calibration.py evaluation_parameters.py --wafer $WAFER --hicann $H
 
 find
 
-find ./build/results_calibration
+find $OUTDIR
+
+unset -v latest_calibration
+for d in $OUTDIR/calibration*; do
+  [[ $d -nt $latest_calibration ]] && latest_calibration=$d
+done
+
+unset -v latest_evaluation
+for d in $OUTDIR/evaluation*; do
+  [[ $d -nt $latest_evaluation ]] && latest_evaluation=$d
+done
+
+bin/tools/make_plots.py $latest_calibration $latest_evaluation $HICANN $OUTDIR/backends --wafer $WAFER --outdir $PLOTDIR
+cp cake/pycake/bin/overview.html $PLOTDIR
