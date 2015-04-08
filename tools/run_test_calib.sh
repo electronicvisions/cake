1ee96f5c24d4e56d311659e1d5b2a5e9ca6cf8e0
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2015-04-08 11:25:54 +0200

    minor reordering

--- tools/run_test_calib.sh
+++ tools/run_test_calib.sh
@@ -6,7 +6,6 @@ OUTDIR=./build/results_calibration
 PLOTDIR=$OUTDIR/plots
 
 mkdir -p $OUTDIR
-mkdir -p $PLOTDIR
 
 # c.f. cake/project.prj
 module load numpy/1.8.0
@@ -21,10 +20,8 @@ cp -v bin/tools/*_parameters.py .
 # issue 1621
 export PYTHONPATH=.:$PYTHONPATH
 
+# run calibration
 bin/tools/run_calibration.py fastcalibration_parameters.py --wafer $WAFER --hicann $HICANN --outdir $OUTDIR
-bin/tools/run_calibration.py evaluation_parameters.py --wafer $WAFER --hicann $HICANN --outdir $OUTDIR
-
-find
 
 find $OUTDIR
 
@@ -33,10 +30,17 @@ for d in $OUTDIR/calibration*; do
   [[ $d -nt $latest_calibration ]] && latest_calibration=$d
 done
 
+# run evaluation
+bin/tools/run_calibration.py evaluation_parameters.py --wafer $WAFER --hicann $HICANN --outdir $OUTDIR
+
+find $OUTDIR
+
 unset -v latest_evaluation
 for d in $OUTDIR/evaluation*; do
   [[ $d -nt $latest_evaluation ]] && latest_evaluation=$d
 done
 
+# make plots
+mkdir -p $PLOTDIR
 bin/tools/make_plots.py $latest_calibration $latest_evaluation $HICANN $OUTDIR/backends --wafer $WAFER --outdir $PLOTDIR
 cp cake/pycake/bin/overview.html $PLOTDIR
