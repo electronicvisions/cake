ed9534df0cd983a8b63470757ce625601b354b64
Dominik Schmidt <dominik.schmidt@kip.uni-heidelberg.de>
2015-04-08 14:33:05 +0200

    Fixed I_pl_Experimentbuilder

--- pycake/experimentbuilder.py
+++ pycake/experimentbuilder.py
@@ -283,7 +283,7 @@ class I_pl_Experimentbuilder(BaseExperimentBuilder):
 
     To be used with hardware."""
     def prepare_specific_config(self, sthal):
-        I_pl_DAC = sthal.get_neuron_parameter(Coordinate.NeuronOnHICANN(Enum(0)), neuron_parameter.I_pl)
+        I_pl_DAC = sthal.hicann.floating_gates.getNeuron(Coordinate.NeuronOnHICANN(Enum(0)), neuron_parameter.I_pl)
         expected_ISI = DACtoHW(I_pl_DAC, neuron_parameter.I_pl) + 1e-8 # estimate 1e-8 as minimum ISI
         # Record at least 50 but not more than 5000 microseconds
         # If errors occur during calibration, these values need to be tuned
