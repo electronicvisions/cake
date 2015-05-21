e1164f1dd0123053adc82ccb40700f25d74de378
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2015-05-21 11:43:02 +0200

    fix unit of refractory period during evaluation
    after restructuring (cf. task #1580), the requested unit during
    evaluation is the unit of the feature, i.e. for tau_refrac it is
    seconds and not anymore the unit of the technical parameter
    
    note that for some parameters, e.g. most potentials, the unit of feature
    and the technical parameter are identical and therefor no change is
    needed
    
    Change-Id: I20fa354f2709925a31ffd9cbc9bc8d717233449e

--- config/sim_eval_parameters.py
+++ config/sim_eval_parameters.py
@@ -2,7 +2,7 @@
 
 import numpy
 
-from pycake.helpers.units import Volt, Ampere
+from pycake.helpers.units import Volt, Ampere, Second
 from pycake.helpers.misc import nested_update
 
 from pyhalbe.HICANN import neuron_parameter
@@ -11,6 +11,9 @@ from pyhalbe.HICANN import shared_parameter
 # original calibration config
 from sim_denmem_parameters import parameters
 
+# Time constants roughly correspond to ideal transformations of:
+# I_pl = [10, 30, 50, 70, 90, 350, 2500] nA
+tau_refs = [4.0e-6, 1.3e-6, 0.8e-6, 0.6e-6, 0.4e-6, 0.1e-6, 0.0]
 
 eval_parameters = {
     "folder_prefix": "sim_evaluation",
@@ -25,7 +28,7 @@ eval_parameters = {
                    neuron_parameter.E_l: Volt(v + 200e-3),
                    neuron_parameter.V_t: Volt(v)} for v in numpy.linspace(700e-3, 1100e-3, 3)],
     "I_gl_charging_range": [{neuron_parameter.I_gl : Ampere(c)} for c in [500e-9, 700e-9]],
-    "I_pl_range": [{neuron_parameter.I_pl : Ampere(c*1e-9)} for c in [10, 30, 50, 70, 90, 2500]],
+    "I_pl_range": [{neuron_parameter.I_pl : Second(t)} for t in tau_refs],
 
     # How many repetitions?
     # Each repetition will take about 1 minute per step!
