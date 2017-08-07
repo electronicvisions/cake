electronicvisions/cake

commit 40fa1cdc093fbab06aa18932ef66c97aacfa44a4

Drop rows containing NaN. Partially fixes issue #2516
Change-Id: I4ca77962fcd20d54caae5afa899e5d98f00cbe0f

pycake/calibrator.py
@@ -86,7 +86,7 @@ class BaseCalibrator(object):
         """
         transformations = {}
         trafo_type = self.get_trafo_type()
-        for neuron, group in self.get_results().groupby(level='neuron'):
+        for neuron, group in self.get_results().dropna(how='any').groupby(level='neuron'):
             # Need to switch y and x in order to get the right fit
             # (y-axis: configured parameter, x-axis: measurement)
             ys_raw, xs_raw = group.T.values
@@ -201,7 +201,7 @@ class V_reset_Calibrator(BaseCalibrator):
             List of tuples, each containing the neuron parameter and a
             dictionary containing polynomial fit coefficients for each neuron
         """
-        results = self.get_results()
+        results = self.get_results().dropna(how='any')
         results_per_FGBlock = results.groupby(level='shared_block')
         trafos = {}
         trafo_type = self.get_trafo_type()
@@ -685,7 +685,7 @@ class I_pl_Calibrator(BaseCalibrator):
         """
         transformations = {}
         trafo_type = self.get_trafo_type()
-        results = self.get_results().groupby(level='neuron')
+        results = self.get_results().groupby(level='neuron').dropna(how='any')
 
         for neuron, group in results:
             # Need to switch y and x in order to get the right fit

commit 1cacd1aa74f7fdaa12bf6c8a48463236c7cc2e7d

Fix dropping NaNs in I_pl_calibrator
I_pl_calibrator has been broken in commit
40fa1cdc093fbab06aa18932ef66c97aacfa44a4, now works again.

Change-Id: Iabea5cf7335f6e4dec539fdeb9f938971f180065

pycake/calibrator.py
@@ -691,7 +691,7 @@ class I_pl_Calibrator(BaseCalibrator):
         """
         transformations = {}
         trafo_type = self.get_trafo_type()
-        results = self.get_results().groupby(level='neuron').dropna(how='any')
+        results = self.get_results().dropna(how='any').groupby(level='neuron')
 
         for neuron, group in results:
             # Need to switch y and x in order to get the right fit