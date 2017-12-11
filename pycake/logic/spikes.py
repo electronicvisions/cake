f9616dc15d9178f5f96b43878960bbc4ed6fbe5a
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2017-12-11 13:19:56 +0100

    Remove deprecated detect_spikes
    Test was failing with:
    
    IndexError: boolean index did not match indexed array
    along dimension 0; dimension is 998 but corresponding
    boolean dimension is 997
    
    Change-Id: I32a96598783490edacd3950710d494e6b3a55fbd

--- pycake/logic/spikes.py
+++ pycake/logic/spikes.py
@@ -2,30 +2,6 @@
 
 import numpy as np
 
-
-def detect_spikes(time, voltage):
-    """DEPRECATED. Use pycake.helpers.peakdetect instead.
-
-    Detect spikes from a voltage trace."""
-
-    # make sure we have numpy arrays
-    t = np.array(time)
-    v = np.array(voltage)
-
-    # Derivative of voltages
-    dv = v[1:] - v[:-1]
-    # Take average over 3 to reduce noise and increase spikes
-    smooth_dv = dv[:-2] + dv[1:-1] + dv[2:]
-    threshhold = -2.5 * np.std(smooth_dv)
-
-    # Detect positions of spikes
-    tmp = smooth_dv < threshhold
-    pos = np.logical_and(tmp[1:] != tmp[:-1], tmp[1:])
-    spikes = t[1:-2][pos]
-
-    return spikes
-
-
 def spikes_to_frequency(spikes):
     """Calculate the spiking frequency from spikes."""
 
--- test/cake_test_logic_spikes.py
+++ test/cake_test_logic_spikes.py
@@ -10,17 +10,6 @@ import numpy as np
 
 
 class TestSpikes(unittest.TestCase):
-    def test_detect(self):
-        """Test spike detection on "nice" trace (without noise)."""
-        time = np.linspace(0, 1000, 1001)
-        voltage = time % 100
-        spike_times = sp.detect_spikes(time, voltage)
-        for t in spike_times:
-            # soft assertion
-            self.assertTrue(voltage[t] > 96)
-            ## hard assertion
-            #self.assertEqual(voltage[t], 99.)
-
     def test_frequency(self):
         spikes = np.array([1, 2, 3, 4, 5, 6, 7, 8])
         self.assertEqual(sp.spikes_to_frequency(spikes), 1)
