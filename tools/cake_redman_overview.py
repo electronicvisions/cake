cdabcb21f1c60f5c4d6cf9465765fe678f8fcec6
Hartmut Schmidt <hschmidt@kip.uni-heidelberg.de>
2020-05-25 18:42:58 +0200

    redman_overview remove duplicate code
    Change-Id: I6665410b0577678d3d80d266906cc672dc41f208

--- tools/cake_redman_overview.py
+++ tools/cake_redman_overview.py
@@ -4,8 +4,7 @@ import pycake.helpers.plotting
 import matplotlib
 from pyhalco_common import iter_all
 import pyhalco_hicann_v2 as C
-import pyredman
-import pyredman.load
+from pyredman import load
 import os.path
 
 import argparse
@@ -14,41 +13,39 @@ parser.add_argument("wafer", type=int, help="wafer enum")
 parser.add_argument("defects_path", type=str, help="path to redman files")
 args = parser.parse_args()
 
-wafer_with_backend = pyredman.load.WaferWithBackend(
+wafer_with_backend = load.WaferWithBackend(
     args.defects_path, C.Wafer(args.wafer), ignore_missing=True)
 
 blacklisted = {}
 jtag = {}
 
-# Format [redman name, halco name (OnHICANN is added later), add text]
-enum_resources = [
-    ["neurons", "Neuron", "True"],
-    ["drivers", "SynapseDriver", "True"],
-    ["synapses", "Synapse", "False"],
-    ["fgblocks", "FGBlock", "True"],
-    ["vrepeaters", "VRepeater", "True"],
-    ["hrepeaters", "HRepeater", "True"],
-    ["synaptic_inputs", "SynapticInput", "True"],
-    ["synapseswitches", "SynapseSwitch", "True"],
-    ["crossbarswitches", "CrossbarSwitch", "True"],
-    ["synapseswitchrows", "SynapseSwitchRow", "True"]]
+# Format [redman name, halco name, add text]
+resources = [
+    ["neurons", "NeuronOnHICANN", True],
+    ["drivers", "SynapseDriverOnHICANN", True],
+    ["synapses", "SynapseOnHICANN", False],
+    ["fgblocks", "FGBlockOnHICANN", True],
+    ["vrepeaters", "VRepeaterOnHICANN", True],
+    ["hrepeaters", "HRepeaterOnHICANN", True],
+    ["synaptic_inputs", "SynapticInputOnHICANN", True],
+    ["synapseswitches", "SynapseSwitchOnHICANN", True],
+    ["crossbarswitches", "CrossbarSwitchOnHICANN", True],
+    ["synapseswitchrows", "SynapseSwitchRowOnHICANN", True],
+    ["hbuses", "HLineOnHICANN", True],
+    ["vbuses", "VLineOnHICANN", True],
+    ["mergers0", "Merger0OnHICANN", True],
+    ["mergers1", "Merger1OnHICANN", True],
+    ["mergers2", "Merger2OnHICANN", True],
+    ["mergers3", "Merger3OnHICANN", True],
+    ["dncmergers", "DNCMergerOnHICANN", True],
+    ["synapserows", "SynapseRowOnHICANN", True],
+    ["analogs", "AnalogOnHICANN", True],
+    ["backgroundgenerators", "BackgroundGeneratorOnHICANN", True]]
 
-non_enum_resources = [
-    ["hbuses", "HLine", "True"],
-    ["vbuses", "VLine", "True"],
-    ["mergers0", "Merger0", "True"],
-    ["mergers1", "Merger1", "True"],
-    ["mergers2", "Merger2", "True"],
-    ["mergers3", "Merger3", "True"],
-    ["dncmergers", "DNCMerger", "True"],
-    ["synapserows", "SynapseRow", "True"],
-    ["analogs", "Analog", "True"],
-    ["backgroundgenerators", "BackgroundGenerator", "True"]]
-
-for res in enum_resources:
-    exec (res[0] + " = {}")
-for res in non_enum_resources:
-    exec (res[0] + " = {}")
+# store blacklisting data for each component and each HICANN
+components = {}
+for res in resources:
+    components[res[0]] = {}
 
 for hicann in iter_all(C.HICANNOnWafer):
     hicann_with_backend = wafer_with_backend.get(hicann)
@@ -65,15 +62,15 @@ for hicann in iter_all(C.HICANNOnWafer):
     if not os.path.exists("{}/hicann-Wafer({})-Enum({}).xml".format(args.defects_path, args.wafer, hicann_id)):
         continue
 
-    blacklisted[hicann_id] = False
-
-    for res in enum_resources + non_enum_resources:
-        exec (
-            "has_value = hicann_with_backend.{}().has_value()".format(res[0]))
+    # check resources
+    for res in resources:
+        has_value = getattr(hicann_with_backend, res[0])().has_value()
         if not has_value:
+            # skip not tested components
             continue
-        exec ("{}[hicann_id] = hicann_with_backend.{}().available()".format(
-            res[0], res[0]))
+        # store number of available components
+        components[res[0]][hicann_id] = getattr(
+            hicann_with_backend, res[0])().available()
 
 # generate JTAG with green yellow
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
@@ -84,29 +81,21 @@ jtag_figure = pycake.helpers.plotting.get_bokeh_figure(
 # generate all other colormaps
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", [
                                                            "green", "red"])
-
 max_value = 1
 blacklisted_figure = pycake.helpers.plotting.get_bokeh_figure(
     "blacklisted", blacklisted, max_value, cmap, default_fill_color='blue')
-
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", [
                                                            "red", "green"])
 
-for res in enum_resources:
-    exec("max_value = C.{}OnHICANN.enum_type.end".format(res[1]))
-    exec("""{}_figure = pycake.helpers.plotting.get_bokeh_figure("{}",{}, max_value, cmap, add_text = {}, default_fill_color = 'blue')""".format(
-        res[0], res[0], res[0], res[2]))
-
-for res in non_enum_resources:
-    exec("max_value = C.{}OnHICANN.end".format(res[1]))
-    exec("""{}_figure = pycake.helpers.plotting.get_bokeh_figure("{}",{}, max_value, cmap, add_text = {}, default_fill_color = 'blue')""".format(
-        res[0], res[0], res[0], res[2]))
-
 figures = [[blacklisted_figure], [jtag_figure]]
-for res in enum_resources:
-    figures.append([eval(res[0] + "_figure")])
-for res in non_enum_resources:
-    figures.append([eval(res[0] + "_figure")])
+for res in resources:
+    # coordinate is either enum or non enum type
+    try:
+        max_value = getattr(C, res[1]).enum_type.end
+    except AttributeError:
+        max_value = getattr(C, res[1]).end
+    figures.append(pycake.helpers.plotting.get_bokeh_figure(
+        res[0], components[res[0]], max_value, cmap, add_text=res[2], default_fill_color='blue'))
 
 pycake.helpers.plotting.store_bokeh("Redman Wafer {} Overview".format(
     args.wafer), figures, "redman_w{}.html".format(args.wafer))
